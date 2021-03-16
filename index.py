593ff70822df06a412d1e8eaffa5c88f3ace2fb7
munchiedunch <70247219+munchiedunch@users.noreply.github.com>
2021-03-15 22:36:13 -0400

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import random
 ans = True
 
 while ans:
-    question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
+    question = input("Ask the magic 8 ball a question: ")
     
     answers = random.randint(1,8)
     
